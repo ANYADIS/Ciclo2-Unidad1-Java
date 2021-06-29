67c59159ab598d78329915302062cbc0dc4cfb66
Angela Robayo <yadisrobayo@gmail.com>
2021-06-29 18:47:07 -0500

    Ciclo2-Unidad1-Java

---
+++ src/App.java
@@ -0,0 +1,27 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        //Enteros
+        int numero = 1;
+        System.out.println(numero);
+        //byte entero8bits = 5;
+        //short entero16bits = 28;
+        //int entero32bits = 1;
+        //long entero64bits = 5;
+        //Decimales
+        float num2 = 1.2f;// 6 o 7 decimales
+        double num3 = 1.355831;// 15 decimales
+        System.out.println(num2);
+        System.out.println(num3);
+        //String
+        String nombre = "Angela";
+        System.out.println(nombre);
+        //Booleanos
+        boolean variable = true;
+        System.out.println(variable);
+        //Lista
+        int [] lista = {1,5,8,6,7};
+        System.out.println(lista[0]);
+        //var Variable = "hola"; // Es tipo str por el "hola"
+        //System.out.println("Hola\tMundo")//alt+92 -->contra slash;
+    }
+}
---
+++ src/explicacion.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class explicacion {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner (System.in);
+        String ingreso = scanner.nextLine(); //lo que guarda es un entero y lo guarda en ingreso
+        //int ingreso = scanner.nextInt(); lo que guarda es un entero y lo guarda en ingreso
+        System.out.println(ingreso);
+
+    }
+}
